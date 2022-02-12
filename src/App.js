ea601b142266b35953cd84125c3af3bc099a3d73
Mohammad Ali <maghorbani759@gmail.com>
2022-02-12 18:04:23 +0330

    [ADD] tailwind css

--- src/App.js
+++ src/App.js
@@ -7,21 +7,8 @@ import store from "./store";
 function App() {
     return (
         <Provider store={store}>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+            <div className="App flex flex-col justify-center items-center">
+                Landing Page
             </div>
         </Provider>
     );
