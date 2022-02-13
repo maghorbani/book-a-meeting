46b25d936e1f509e155efd56c20aed12b83cbc7e
Mohammad Ali <maghorbani759@gmail.com>
2022-02-13 18:06:59 +0330

    [ADD] google sheets

--- routers/api.js
+++ routers/api.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var axios = require("axios");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 router.post("/sendEmail", async function (req, res, next) {
     const email = req.body.email;
@@ -13,6 +14,18 @@ router.post("/sendEmail", async function (req, res, next) {
                 },
             }
         );
+        const creds = require("../.config/" + process.env.GOOGLE_CRED_FILE);
+        const doc = new GoogleSpreadsheet(
+            "1mUWQ3zWHDbOHrV308sheHdU6T__S_mpGvUzdvDpKeC8"
+        );
+
+        await doc.useServiceAccountAuth(creds);
+
+        await doc.loadInfo();
+
+        const sheet = await doc.sheetsByIndex[0];
+        // const rows = await sheet.getRows();
+
         const emails = oncehub_resp.data.data.map(function (element) {
             return element["form_submission"]["email"];
         });
@@ -35,9 +48,33 @@ router.post("/sendEmail", async function (req, res, next) {
                 automation: "2",
             },
         };
+        sheetRow = {
+            email: email,
+            name: "",
+            phone: "",
+            "mobile phone": "",
+            note: "",
+            company: "",
+            guests: "",
+            "join url": "",
+            "start time": "",
+            "duration(minutes)": "",
+        };
         if (emails.includes(email)) {
             data.contactAutomation.automation = "1";
+            const ind = emails.indexOf(email);
+            const rowData = oncehub_resp.data.data[ind];
+            sheetRow.name = rowData.form_submission.name;
+            sheetRow.email = rowData.form_submission.email;
+            sheetRow.phone = rowData.form_submission.phone;
+            sheetRow["mobile phone"] = rowData.form_submission.mobile_phone;
+            sheetRow.note = rowData.form_submission.note;
+            sheetRow.company = rowData.form_submission.company;
+            sheetRow.note = rowData.form_submission.note;
+            sheetRow["start time"] = rowData.starting_time;
+            sheetRow["duration(minutes)"] = rowData.duration_minutes;
         }
+        await sheet.addRow(sheetRow);
         let add_to_automation = await axios.post(
             process.env.CAMPAIGN_URL + "contactAutomations",
             data,
@@ -47,25 +84,28 @@ router.post("/sendEmail", async function (req, res, next) {
                 },
             }
         );
+
         res.json(add_to_automation.data);
     } catch (error) {
-        console.log(error);
-        res.send(error.response.data);
+        console.log(error.response.data);
+        res.json("");
     }
 });
 /* GET home page. */
-router.post("/contacts", async function (req, res, next) {
-    console.log(req.body);
-    axios
-        .post(process.env.CAMPAIGN_URL + "contacts", req.body, {
-            headers: {
-                "api-token": process.env.CAMPAIGN_API_TOKEN,
-            },
-        })
-        .then((data) => {
-            res.json(data.data);
-        })
-        .catch((err) => res.status(401).json({ message: err.message }));
+router.get("/sheet", async function (req, res, next) {
+    try {
+        const creds = require("../" + process.env.GOOGLE_CRED_FILE);
+        const doc = new GoogleSpreadsheet(
+            "1mUWQ3zWHDbOHrV308sheHdU6T__S_mpGvUzdvDpKeC8"
+        );
+
+        await doc.useServiceAccountAuth(creds);
+
+        await doc.loadInfo();
+        console.log(doc.title);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 module.exports = router;
